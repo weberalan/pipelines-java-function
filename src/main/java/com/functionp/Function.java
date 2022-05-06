038b03fe934496229fdc85a908a7f03f6f6482f4
weberalan <103123771+weberalan@users.noreply.github.com>
2022-05-07 01:36:59 +0200

    Update Function.java

--- src/main/java/com/functionp/Function.java
+++ src/main/java/com/functionp/Function.java
@@ -28,7 +28,7 @@ public class Function {
         if (name == null) {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Wow it's finally working, " + name).build();
+            return request.createResponseBuilder(HttpStatus.OK).body("Wow, yeah... it's finally working, " + name).build();
         }
     }
 }
